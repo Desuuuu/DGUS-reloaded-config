a79cc2efa69e3cc75361048a589b018484a755b4
Desuuuu <contact@desuuuu.com>
2020-07-21 03:45:18 +0200

    Use classic jerk instead of junction deviation

--- examples/Creality/CR10SProV2/Configuration.h
+++ examples/Creality/CR10SProV2/Configuration.h
@@ -777,11 +777,11 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-//#define CLASSIC_JERK
+#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
   #define DEFAULT_XJERK 10.0
   #define DEFAULT_YJERK 10.0
-  #define DEFAULT_ZJERK  0.3
+  #define DEFAULT_ZJERK  0.4
 
   //#define TRAVEL_EXTRA_XYJERK 0.0     // Additional jerk allowance for all travel moves
 
